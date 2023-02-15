07cc64f9fb136067b3384a10cb26261a1ecc57c4
claudia berlian harli <claudia.berlian.tif22@polban.ac.id>
2023-02-15 19:10:46 +0700

    selesai 15/02/2023

--- main claudia.c
+++ main claudia.c
@@ -1,19 +1,148 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "claudia.h"
 
-int main(){
-//deklarasi
- float a,b;
- float hasil;
- //input
-    printf("Masukan nilai petama: ",a);
-    scanf("%g",&a);
-    printf("Masukakan nilai kedua: ",b);
-    scanf("%g",&b);
-    hasil=a-b;
-    printf("\n%g - %g = %g",a,b,hasil);
     
-return 0 ;
-}
+int main(){
+
+	//pengurangan
+	float a, b,hasil;
+    printf("Masukkan angka pertama: ");
+    scanf("%g", &a);
+    printf("masukan angka kedua: ");
+    scanf("%g",&b);
+     
+     hasil=a-b;
+     printf(" %g - %g= %g\n",a,b,hasil);
+     
+
+	 //cosinus
+     printf("masukan angka pertama :");
+     scanf("%g",&a);
+     printf("masukan angka kedua: ");
+     scanf("%g",&b);
+      printf("cos(%.2lg) = %.2lg\n", a, cos(a));
+      
+     
+	  //logaritma natural
+      printf("masukan angka pertama: ");
+      scanf("%g",&a);
+      printf("masukan angka kedua: ");
+      scanf("%g",&b);
+        hasil=log(a);
+        printf("ln(%1g)=%1g\n",a,hasil);
+     
 	
+	 //matriks   
+  int baris, kolom, i, j, matrik1[10][10], matrik2[10][10],jumlah[10][10];
+ 
+   printf("Input jumlah baris matrik : "); 
+   scanf("%d", &baris);
+   printf("Input jumlah kolom matrik : ");
+    scanf("%d", &kolom);
+   
+   printf("nInput nilai matrik 1 : \n");
+   for (i = 0; i < baris; i++){
+      for (j = 0; j < kolom; j++){
+      printf("Input Matriks 1[%d][%d]: ",i+1,j+1);
+  scanf("%d",&matrik1[i][j]);
+      }
+   }
+   
+   printf("\n Matrik 1 : \n");
+   for (i=0; i< baris; i++){
+    for (j=0; j<kolom; j++){
+     printf("%3d", matrik1[i][j]);
+    }
+    printf("\n");
+   }printf("\nInput nilai matrik 2 :\n");
+   for (i = 0; i < baris; i++){    
+      for (j = 0 ; j < kolom; j++){
+    printf("Input Matriks 2[%d][%d]: ",i+1,j+1);
+  scanf("%d",&matrik2[i][j]);
+      }
+   }
+   
+   printf("\n Matrik 2 : \n");
+   for (i=0; i< baris; i++){
+    for (j=0; j<kolom; j++){
+     printf("%3d", matrik2[i][j]);
+    }
+    printf("\n");
+   }
+    
+   
+	   printf("\nHasil Penjumlahan Matrik :\n");
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] + matrik2[i][j];
+         printf("%d\t", jumlah[i][j]);}
+         printf("\n"); 
+    }
+       printf("\nHasil Pengurangan Matrik :\n");
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] - matrik2[i][j];
+         printf("%d\t", jumlah[i][j]);}
+         printf("\n");
+        }
+      printf("\nHasil Perkalian Matrik :\n");
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] * matrik2[i][j];
+         printf("%d\t", jumlah[i][j]);}
+         printf("\n");
+   }
+       printf("\nHasil Pembagian Matrik :\n");
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] + matrik2[i][j];
+         printf("%d\t", jumlah[i][j]);}
+         printf("\n");
+    }
+    
+ 
+    //invers
+ int c[5][5];
+ int x,y,det;
+
+ printf("masukkan jumlah kolom matriks : ");
+ scanf("%d",&x);
+ printf("masukkan jumlah baris matriks : ");
+ scanf("%d",&y); 
+
+ for (i=0;i<2;i++){
+ 	for (j=0;j<2;j++){
+ 		printf("masukkan elemen [%d] [%d ]: ",i+1,j+1);
+        scanf("%d",&c[i][j]);
+	 }
+ }
+ printf("Matriks A: \n");
+ for (i=0; i< 2; i++){
+    for (j=0; j<2; j++){
+     printf("%3d", c[i][j]);
+    }
+    printf("\n");
+}
+det = (c[1][1]*c[2][2]-c[2][1]*c[1][2]);
+while(hasil !=0)
+ hasil = 1/det;
+
+  c[1][1] = hasil*c[2][2];
+  c[1][2] = hasil*c[2][1];
+  c[2][1] = hasil*c[1][2];
+  c[1][2] = hasil*c[1][1];
+
+
+  printf("invers matriks \n");
+   for (i=0; i< 2; i++){
+      for (j=0; j<2; j++){
+     printf("%3d", c[i][j]);
+     }
+     printf("\n");
+	}
+  
+    return 0;
+}
+    
