2d69a2c6126158b7291e05b43221ad4247248476
Quineeryn <canandraeka10@gmail.com>
2023-05-20 13:13:06 +0700

    ini adalah update

--- Canandra.c
+++ Canandra.c
@@ -1,27 +1,62 @@
 #include "Canandra.h"
 #include "Rahaditya.h"
 
-//--------------perkalian
+
 double perkalian (double x, double y) {
 	return x * y;
 }
+// IS :   - Fungsi perkalian didefinisikan dengan tipe pengembalian double dan dua parameter x dan y dengan tipe double.//
+		//- Fungsi ini belum dieksekus//
+		
+// FS :   - Fungsi perkalian berhasil didefinisikan.
+       // - Fungsi ini siap digunakan dan akan mengembalikan hasil perkalian dari x dan y dengan tipe double ketika dipanggil.//
+       
+// Referensi : www.
+
+// Pembuat : Canandra Eka Mukti
 
 double sinus (double x) {
 	x = (x * phi / 180.0);
     x = sin (x);
 	return x;
 }
+// IS :	 -  Fungsi sinus didefinisikan dengan tipe pengembalian double dan satu parameter x dengan tipe double.//
+	  // -  Fungsi ini belum dieksekusi.//
+	  
+// FS :	 - Fungsi sinus berhasil didefinisikan.
+	  // - Fungsi ini siap digunakan dan akan mengembalikan nilai sinus dari x yang dikonversi menjadi radian menggunakan konstanta 
+	  //   phi ketika dipanggil.//
+	  
+// Referensi	:www.
+
+// Pembuat		: Canandra Eka Mukti
 
 double cosecan (double x) {
 	x = (x * phi / 180.0);
     x = 1 / sin (x);
 	return x;
 }
+// IS :  - Fungsi cosecan didefinisikan dengan tipe pengembalian double dan satu parameter x dengan tipe double.//
+      // - Fungsi ini belum dieksekusi.//
+      
+// FS :  - Fungsi cosecan berhasil didefinisikan.
+     //  - Fungsi ini siap digunakan dan akan mengembalikan nilai cosecan dari x yang dikonversi menjadi radian menggunakan konstanta 
+	 //	   phi ketika dipanggil.//
+	 
+// Referensi	: www.
+// Pembuat 		: Canandra Eka Mukti
 
-//------------log basis 10
 double logbase10 (double x) {
 	return log (x) / 2.30258509299404568402;
 }
+// IS :  - Fungsi logbase10 didefinisikan dengan tipe pengembalian double dan satu parameter x dengan tipe double.//
+	 //  - Fungsi ini belum dieksekusi.//
+	 
+// FS :  - Fungsi logbase10 berhasil didefinisikan.//
+	 //	 - Fungsi ini siap digunakan dan akan mengembalikan logaritma basis 10 dari x ketika dipanggil.//
+	 
+// Referensi	: www.
+// Pembuat 		: Canandra Eka Mukti
 
 double LN (double x) {
     double old_sum = 0.0;
@@ -40,8 +75,15 @@ double LN (double x) {
     }
     return 2.0 * sum;
 }
+// IS :   - Fungsi LN didefinisikan dengan tipe pengembalian double dan satu parameter x dengan tipe double.//
+	 //	  - Fungsi ini belum dieksekusi.//
+	 
+// FS :   - Fungsi LN berhasil didefinisikan.//
+	 //	  - Fungsi ini siap digunakan dan akan mengembalikan nilai logaritma natural dari x ketika dipanggil.//
 
-//-------------------------diskriminan
+// Referensi	: www. 
+// Pembuat 		: Canandra Eka Mukti
+
 double diskriminan (double x, double y, double z) {
 	double Diskriminan, akar1, akar2, real, imajiner;
 	
@@ -65,8 +107,16 @@ double diskriminan (double x, double y, double z) {
 	}
 	
 }
+// IS :	  - Fungsi diskriminan didefinisikan dengan tipe pengembalian double dan tiga parameter x, y, dan z, semuanya memiliki tipe double.
+	 //	  - Variabel lokal Diskriminan, akar1, akar2, real, dan imajiner dideklarasikan.//
+	 //   - Fungsi ini belum dieksekusi.//
+	 
+// FS :   - Fungsi diskriminan berhasil didefinisikan.//
+     //   - Fungsi ini siap digunakan untuk menghitung dan menampilkan akar-akar persamaan kuadrat berdasarkan diskriminannya ketika dipanggil.//
 
-//--------------------------permutasi
+// Referensi		: www.
+// Pembuat 			: Canandra Eka Mukti
+
 double faktorialPermutasi(double x) {
 	if (x==0 || x==1){
 		return 1;
@@ -75,11 +125,36 @@ double faktorialPermutasi(double x) {
 		return x * faktorialPermutasi(x-1);
 	}
 }
+// IS :   - Fungsi faktorialPermutasi didefinisikan dengan tipe pengembalian double dan satu parameter x dengan tipe double.//
+	 //   - Fungsi ini belum dieksekusi.//
+	 
+// FS :   - Fungsi faktorialPermutasi berhasil didefinisikan.//
+     //   - Fungsi ini siap digunakan untuk menghitung faktorial permutasi dari x ketika dipanggil.//
+
+// Referensi 	: www.
+// Pembuat 		: Canandra Eka Mukti
 
 double permutasi(double x, double y) {
 	return faktorialPermutasi(x)/faktorialPermutasi(x-y);
 }
+// IS  :  - Fungsi permutasi didefinisikan dengan tipe pengembalian double dan dua parameter x dan y, keduanya memiliki tipe double.//
+     //   - Fungsi ini menggunakan fungsi faktorialPermutasi yang sudah didefinisikan sebelumnya.//
+     //   - Fungsi ini belum dieksekusi.//
+     
+// FS  :  - Fungsi permutasi berhasil didefinisikan.//
+	 //   - Fungsi ini siap digunakan dan akan mengembalikan hasil permutasi dari x elemen dengan y elemen yang diambil tanpa pengembalian ketika dipanggil.//
+		
+// Referensi	: www.
+// Pembuat		: Canandra Eka Mukti
 
 double lingkaran(double x) {
 	return phi*x*x;
 }
+// IS :   - Fungsi lingkaran didefinisikan dengan tipe pengembalian double dan satu parameter x dengan tipe double.//
+     //    - Fungsi ini belum dieksekusi.//
+    
+// FS :   - Fungsi lingkaran berhasil didefinisikan.//
+     //Fungsi ini siap digunakan dan akan mengembalikan luas lingkaran dengan jari-jari x ketika dipanggil.//
+
+// Referensi	: www.
+// Pembuat 		: Canandra Eka Mukti
