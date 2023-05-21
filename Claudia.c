906d889a54e434aa8b55625e5f860183a247a131
berlian16-BH <123914486+berlian16-BH@users.noreply.github.com>
2023-05-21 19:17:16 +0700

    updatee

--- Claudia.c
+++ Claudia.c
@@ -1,299 +1,154 @@
-#include "Claudia.h"
-#include "Rahaditya.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "claudia.h"
+#include <stdio.h>
 
-double pengurangan (double x, double y) {
-	return x - y;
+
+float pengurangan(float a,float b){
+
+	return a-b;
+}
+ 
+//modul logaritma natural
+double pangkat(double base, int exponent) {
+    double result = 1;
+    for (int i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
 }
 
-double cosinus (double x) {
-	return cos ( (x * phi) / 180 );
+double ln(double x) {
+    double epsilon = 0.00000001;
+    double result = 0.0;
+    double numerator = x - 1;
+    double denominator = 1.0;
+    int i = 1;
+    while (numerator / denominator > epsilon) {
+        result += numerator / denominator;
+        numerator = -numerator * (x - 1);
+        denominator = ++i;
+    }
+    return result;
 }
 
-double secan (double x) {
-	return 1 / cos ( (x * phi) / 180 );
+int  matrikspenjumlahan (int baris, int kolom){
+    int i, j, matrik1[10][10], matrik2[10][10],jumlah[10][10];
+
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] + matrik2[i][j];
+    }
+    }
+    return jumlah[i][j];
+}
+int  matrikspengurangan  (int baris, int kolom){
+    int i, j, matrik1[10][10], matrik2[10][10],jumlah[10][10];
+      
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] - matrik2[i][j];
+        }
+    }
+    return jumlah[i][j];
+    }
+int  matriksperkalian  (int baris, int kolom){
+    int i, j, matrik1[10][10], matrik2[10][10],jumlah[10][10];
+      
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] * matrik2[i][j];
+        }
+
+   }
+      return jumlah[i][j];
+}
+int  matrikspembagian (int baris, int kolom){
+    int i, j, matrik1[10][10], matrik2[10][10],jumlah[10][10];
+       
+       for (i = 0; i < baris; i++) {
+       for (j = 0 ; j < kolom; j++) {
+         jumlah[i][j] = matrik1[i][j] + matrik2[i][j];
+        }
+    }
+    return jumlah[i][j];
 }
 
-double ln (double x) {
-    double hasil;
-    hasil = log (x);
-    return hasil;
+double invers(double bilangan, double invers){
+
+    printf("Masukkan bilangan: ");
+    scanf("%lf", &bilangan);
+
+    invers = 1.0 / bilangan;
+
+    return bilangan, invers;
 }
 
-int matriksPenjumlahan (int baris, int kolom) {
-	int i, j, matrik1[10][10], matrik2[10][10], jumlah[10][10];
-	tampilanStruktur (2);
-    printf("Nilai matrik 1 : \n");
-	for (i = 0; i < baris; i++){
-		for (j = 0; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 1[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik1[i][j]);
-		}
-	}
-	printf ("\n");
-	tampilanStruktur (2);
-	printf("Matrik 1 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik1[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-    printf("Nilai matrik 2 : \n");
-	for (i = 0; i < baris; i++){    
-		for (j = 0 ; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 2[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik2[i][j]);
-		}
-	}
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Matrik 2 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik2[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Hasil Penjumlahan Matrik :\n\n");
-	for (i = 0; i < baris; i++) {
-		tampilanStruktur (2);
-		for (j = 0 ; j < kolom; j++) {
-			jumlah[i][j] = matrik1[i][j] + matrik2[i][j];
-			printf("%d\t", jumlah[i][j]);
-		}
-		printf("\n"); 
+//  moduL cosinus
+double to_radians(double degrees) {
+    return degrees * 0.0174533;
+}
+
+double power(double base, int exponent) {
+    double result = 1.0;
+    for (int i = 1; i <= exponent; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+double factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
     }
 }
 
-int matriksPengurangan  (int baris, int kolom) {
-    int i, j, matrik1[10][10], matrik2[10][10], jumlah[10][10];
-	tampilanStruktur (2);
-    printf("Nilai matrik 1 : \n");
-	for (i = 0; i < baris; i++){
-		for (j = 0; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 1[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik1[i][j]);
-		}
-	}
-	printf ("\n");
-	tampilanStruktur (2);
-	printf("Matrik 1 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik1[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-    printf("Nilai matrik 2 : \n");
-	for (i = 0; i < baris; i++){    
-		for (j = 0 ; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 2[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik2[i][j]);
-		}
-	}
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Matrik 2 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik2[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Hasil Pengurangan Matrik :\n\n");
-	for (i = 0; i < baris; i++) {
-		tampilanStruktur (2);
-		for (j = 0 ; j < kolom; j++) {
-			jumlah[i][j] = matrik1[i][j] - matrik2[i][j];
-			printf("%d\t", jumlah[i][j]);
-		}
-		printf("\n"); 
+double cosine(double degrees) {
+    double radians = to_radians(degrees);
+    double cos = 1.0;
+    int sign = -1;
+    double term;
+    for (int i = 2; i <= 20; i += 2) {
+        term = power(radians, i) / factorial(i);
+        cos += sign * term;
+        sign = -sign;
+    }
+    return cos;
+}
+
+// modul secan
+double to_radians1(double degrees) {
+    return degrees * 0.0174533;
+}
+
+double power1(double base, int exponent) {
+    double result = 1.0;
+    for (int i = 1; i <= exponent; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+double factorial1(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial1(n - 1);
     }
 }
 
-int matriksPerkalian  (int baris, int kolom) {
-    int i, j, matrik1[10][10], matrik2[10][10], jumlah[10][10];
-	tampilanStruktur (2);
-    printf("Nilai matrik 1 : \n");
-	for (i = 0; i < baris; i++){
-		for (j = 0; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 1[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik1[i][j]);
-		}
-	}
-	printf ("\n");
-	tampilanStruktur (2);
-	printf("Matrik 1 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik1[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-    printf("Nilai matrik 2 : \n");
-	for (i = 0; i < baris; i++){    
-		for (j = 0 ; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 2[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik2[i][j]);
-		}
-	}
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Matrik 2 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik2[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Hasil Perkalian Matrik :\n\n");
-	for (i = 0; i < baris; i++) {
-		tampilanStruktur (2);
-		for (j = 0 ; j < kolom; j++) {
-			jumlah[i][j] = matrik1[i][j] * matrik2[i][j];
-			printf("%d\t", jumlah[i][j]);
-		}
-		printf("\n"); 
+double secan(double degrees1) {
+    double radians = to_radians1(degrees1);
+    double cos = 1.0;
+    int sign = -1;
+    double term;
+    for (int i = 2; i <= 20; i += 2) {
+        term = power1(radians, i) / factorial1(i);
+        cos += sign * term;
+        sign = -sign;
     }
+    return 1/cos;
 }
-int matriksPembagian (int baris, int kolom) {
-    int i, j, matrik1[10][10], matrik2[10][10], jumlah[10][10];
-	tampilanStruktur (2);
-    printf("Nilai matrik 1 : \n");
-	for (i = 0; i < baris; i++){
-		for (j = 0; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 1[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik1[i][j]);
-		}
-	}
-	printf ("\n");
-	tampilanStruktur (2);
-	printf("Matrik 1 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik1[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-    printf("Nilai matrik 2 : \n");
-	for (i = 0; i < baris; i++){    
-		for (j = 0 ; j < kolom; j++){
-			tampilanStruktur (0);
-			printf("Input Matriks 2[%d][%d] : ",i+1,j+1);
-			scanf("%d",&matrik2[i][j]);
-		}
-	}
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Matrik 2 : \n\n");
-	for (i=0; i< baris; i++){
-		tampilanStruktur (2);
-		for (j=0; j<kolom; j++){
-			printf("%3d", matrik2[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	tampilanStruktur (4);
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Hasil Pembagian Matrik :\n\n");
-	for (i = 0; i < baris; i++) {
-		tampilanStruktur (2);
-		for (j = 0 ; j < kolom; j++) {
-			jumlah[i][j] = matrik1[i][j] / matrik2[i][j];
-			printf("%d\t", jumlah[i][j]);
-		}
-		printf("\n"); 
-    }
-}
-
-int invers() {
-	int c[2][2]={1,2,3,4};
-	int x,y,det, i, j; 
-	 
-	for (i=0; i< 2; i++){
-	    for (j=0; j<2; j++){
-			tampilanStruktur (0);
-			printf("masukkan elemen [%d] [%d] : ",i+1,j+1);
-        	scanf("%d",&c[i][j]);
-	    }
-//	    return c[i][j];
-	}
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Matriks A: \n");
-	for (i=0; i< 2; i++){
-		tampilanStruktur (2);
-    	for (j=0; j<2; j++){
-    		printf("%3d", c[i][j]);
-    	}
-    	printf("\n");
-	}
-	
-	det=c[0][0]*c[1][1]-c[1][0]*c[0][1];
-	int A2[2][2];
-	A2[1][1]=c[0][0];
-	A2[0][0]=c[1][1];
-	A2[0][1]=c[0][1]*-1;
-	A2[1][0]=c[1][0]*-1;
-	A2[1][1]=c[0][0];
-	A2[0][0]=c[1][1];
-	A2[0][1]=c[0][1]*-1;
-	A2[1][0]=c[1][0]*-1;
-	
-	printf("\n");
-	tampilanStruktur (2);
-	printf("Invers: \n");
-	for (i=0; i< 2; i++){
-		tampilanStruktur (2);
-    	for (j=0; j<2; j++){
-    		printf("%3d",  A2[i][j]/det);
-    	}
-    	printf("\n");
-	}
-	return A2[i][j]/det;
-}
-
