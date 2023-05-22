e1d44910eda2be1b53b212043b4c9a2b96362bf6
Quineeryn <110579948+Quineeryn@users.noreply.github.com>
2023-05-22 15:58:40 +0700

    Add files via upload

--- Canandra.c
+++ Canandra.c
@@ -1,148 +1,91 @@
 #include "Canandra.h"
-#include <stdio.h>
-#define n 50
-#define LN10 2.3025850929940456840179914546844
-#define phi 22/7
+#include "Rahaditya.h"
+#include "Hafidzon.h"
 
-	//MENu
-	void menu()
-	{
-		printf("1.Perkalian\n");
-		printf("2.Sin\n");
-		printf("3.Cosec\n");
-		printf("4.Logaritma\n");
-		printf("5.Diskriminan\n");
-		printf("6.Permutasi\n");
-		printf("7.Lingkaran");
-	}
+//--------------perkalian
+double perkalian (double x, double y) {
+	return x * y;
+}
 
-	
-	//--------------prekalian
-	float perkalian(float x, float y)
-	{
+double sinus (double x) {
+	x = (x * phi / 180.0);
+	double sin = 0;
 
-		float hasil;
-		return hasil = x*y;
+	int i;
+	for(i = 0; i <= 10; i++) {
+		sin = sin + (power(-1, i) * power(x, 2 * i + 1) / facttan(2 * i + 1));
 	}
-	
-		//---------sin
-		float sinus(float x)
-	{
-		float hasil;
-		float faktorial;
-		float pangkat;
-		int i,j;
-		
-		hasil = 0.0;
-		for( i = 0; i <= n; i++)
-		{
-			faktorial = 1.0;
-			pangkat = 1.0;
-			for(j = 1; j <= 2*i+1; j++)
-			{
-				faktorial *= j;
-				pangkat *= x;
-			}
-			hasil += ((i%2?-1.0:1.0)/faktorial)*pangkat;
-		}
-		return hasil;
-	}
-	
-	//------------cosec
-	float cosec(float x)
-	{
-		float hasil;
-		float faktorial;
-		float pangkat;
-		int i,j;
+	return sin;
+}
 
-		hasil = 0.0;
-		for( i = 0; i <= n; i++)
-		{
-			faktorial = 1.0;
-			pangkat = 1.0;
-			for(j = 1; j <= 2*i+1; j++)
-			{
-				faktorial *= j;
-				pangkat *= x;
-			}
-			hasil += ((i%2?-1.0:1.0)/faktorial)*pangkat;
-		}
-		return hasil;
-	}
-	
-	//------------log baasis 10
-	float logbase10(float x)
-	{
-    	return ln(x) / LN10;
-	}
+double cosecan (double x) {
+	x = (x * phi / 180.0);
+    x = 1 / sinus (x);
+	return x;
+}
 
-	float ln(float x)
-	
-	{
-	    float old_sum = 0.0;
-	    float xmlxpl = (x - 1) / (x + 1);
-	    float xmlxpl_2 = xmlxpl * xmlxpl;
-	    float denom = 1.0;
-	    float frac = xmlxpl;
-	    float term = frac;
-	    float sum = term;
+//------------log basis 10
+double logbase10 (double x) {
+	return log (x) / 2.30258509299404568402;
+}
 
-	    while ( sum != old_sum )
-	    {
-	        old_sum = sum;
-	        denom += 2.0;
-	        frac *= xmlxpl_2;
-	        sum += frac / denom;
-	    }
-	    return 2.0 * sum;
+double LN (double x) {
+    double old_sum = 0.0;
+    double xmlxpl = (x - 1) / (x + 1);
+    double xmlxpl_2 = xmlxpl * xmlxpl;
+    double denom = 1.0;
+    double frac = xmlxpl;
+    double term = frac;
+    double sum = term;
+
+    while ( sum != old_sum ) {
+        old_sum = sum;
+        denom += 2.0;
+        frac *= xmlxpl_2;
+        sum += frac / denom;
+    }
+    return 2.0 * sum;
+}
+
+//-------------------------diskriminan
+double diskriminan (double x, double y, double z) {
+	double Diskriminan, akar1, akar2, real, imajiner;
+	
+	Diskriminan = x*x - 4 * y * z;
+	if (Diskriminan == 0) {
+		akar1 = akar2 = -x/(2*y);
+		tampilanStruktur (2);
+		printf("akar1 = akar2 = %.2lf\n", akar1);
+	}
+	else if(Diskriminan > 0) {
+		akar1 = (-x + sqrt(Diskriminan))/(2*y);
+		akar2 = (-x - sqrt(Diskriminan))/(2*y);
+		tampilanStruktur (2);
+		printf("akar 1 = %.2lf dan akar 2 = %.lf\n", akar1, akar2);
+	}
+	else {
+		real = -x/(2*y);
+		imajiner = sqrt(-Diskriminan)/(2*z);
+		tampilanStruktur (3);
+		printf("akar 1 = %.2lf + %.2lfi dan akar 2 = %.2f-%.2fi\n", real, imajiner, real, imajiner);
 	}
 	
-	//-------------------------diskriminan
-	float diskriminan(float x, float y, float z)
-	{
-		float Diskriminan;
-		float akar1, akar2, real, imajiner;
-		
-		Diskriminan = x*x - 4 * y * z;
-		if (Diskriminan == 0)
-		{
-			akar1 = akar2 = -x/(2*y);
-			printf("akar1 = akar2 = %.2lf", akar1);
-		}
-		else if(Diskriminan > 0)
-			{
-				akar1 = (-x + sqrt(Diskriminan))/(2*y);
-				akar2 = (-x - sqrt(Diskriminan))/(2*y);
-				printf("akar 1 = %.2lf dan akar 2 = %.lf", akar1, akar2);
-			}
-			else
-			{
-				real = -x/(2*y);
-				imajiner = sqrt(-Diskriminan)/(2*z);
-				printf("akar 1 = %.2lf + %.2lfi dan akar 2 = %.2f-%.2fi", real, imajiner, real, imajiner);
-			}
-		
+}
+
+//--------------------------permutasi
+double faktorialPermutasi(double x) {
+	if (x==0 || x==1){
+		return 1;
 	}
-	
-	//--------------------------permutasi
-	float faktorial(float x)
-	{
-		if (x==0 || x==1)
-		{
-			return 1;
-		}
-		else
-		
-			return x * faktorial(x-1);
+	else{
+		return x * faktorialPermutasi(x-1);
 	}
-	
-	float permutasi(float x, float y)
-	{
-		return faktorial(x)/faktorial(x-y);
-	}
-	
-	float lingkaran(float x)
-	{
-		return phi*x*x;
-	}
+}
+
+double permutasi(double x, double y) {
+	return faktorialPermutasi(x)/faktorialPermutasi(x-y);
+}
+
+double lingkaran(double x) {
+	return phi*x*x;
+}
