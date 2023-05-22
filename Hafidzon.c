5fa8ca04fdfa0810bcecf42643cf5d9bdc582c3d
Hafidzon Al Hibrizi <117968719+haalzi@users.noreply.github.com>
2023-05-22 16:04:35 +0700

    Add files via upload

--- Hafidzon.c
+++ Hafidzon.c
@@ -4,43 +4,68 @@
 double pembagian(double x, double y) {
 	return x / y;
 }
-
-double eksponen (double x, int y) {
-	return pow(x, y);
+double eksponen(double x,int y) {
+	double pow = 1;
+	int i;
+	for (i = 1; i <= y; i++) {
+		pow = pow * x;
+	}
+	return pow;
 }
-
-double faktorial (double x) {
-	int y;
+double faktorial(double x) {
 	double fact = 1;
-    for (y = 1; y <= x; y++) {
-    	fact = fact * y; 
+	int i;
+	for (i = 1; i <= x; i++) {
+		fact = fact * i;
 	}
 	return fact;
 }
 
-double tangen (double x) {
+double tangen(double x) {
 	x = (x * phi / 180.0);
-    x = tan (x);
-	return x;
+	return (sinuss(x)/cosinuss(x));
 }
 
-double cotangen (double x) {
-	x = (x * phi / 180.0);
-    x = 1 / tan (x);
-	return x;
+double cotangen(double x){
+	return (1/tangen(x));
 }
 
+double power(double base, int exp) {
+	if(exp < 0) {
+		if(base == 0)
+			return -0;
+		return 1 / (base * power(base, (-exp) - 1));
+	}
+	if(exp == 0)
+		return 1;
+	if(exp == 1)
+		return base;
+	return base * power(base, exp - 1);
+}
+
+int facttan(int n) {
+	return n <= 0 ? 1 : n * facttan(n-1);
+}
+
+double sinuss(double deg) {
+	double sin = 0;
+
+	int i;
+	for(i = 0; i <= 10; i++) {
+		sin = sin + (power(-1, i) * power(deg, 2 * i + 1) / facttan(2 * i + 1));
+	}
+	return sin;
+}
+
+double cosinuss(double deg) {
+	double cos = 0;
+
+	int i;
+	for(i = 0; i <= 10; i++) {
+		cos = cos + (power(-1, i) * power(deg, 2 * i) / facttan(2 * i));
+	}
+	return cos;
+}
 double mutlak (double x) {
 	return (x < 0) ? -x : x;
-}
-
-double sinusS(double x) {
-	x = (x * 3.14159265358979323846 / 180.0);
-    double term = x, sum = 0.0;
-    int i;
-    for (i = 1; i <= 10; i++) {
-        sum += term;
-        term *= (-1) * x * x / ((2 * i) * (2 * i + 1));
-    }
-    return sum;
 }
